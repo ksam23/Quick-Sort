1e3ef000e8bf41dcaf734a0bae9abf3a03e4d054
ksamlihan <ksamlihan@cs.binghamton.edu>
2026-02-02 22:27:17 -0500

    Main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+#include <vector>
+#include "QuickSort.h"
+using namespace std;
+
+int main(){
+    int total;
+    cin >> total;
+
+    vector<int> arr(total);
+    for(int i = 0; i < total; i++){
+        cin >> arr[i];
+    }
+
+    quickSort(arr, 0, total - 1);
+
+    cout << "Total: "<< total << endl;
+    for(int i = 0; i < total; i++){
+        cout << arr[i] << endl;
+    }
+}
